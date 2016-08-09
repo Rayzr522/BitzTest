41bb2d977ae7249d9eb685060f817477d5aeefa9
Rayzr522 <rayzr522@users.noreply.github.com>
2016-08-09 11:40:19 -0400

    Simple command system (with child command)

--- src/main/java/com/rayzr522/bitztest/BitzTest.java
+++ src/main/java/com/rayzr522/bitztest/BitzTest.java
@@ -1,6 +1,7 @@
 
 package com.rayzr522.bitztest;
 
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.rayzr522.bitzapi.BitzPlugin;
@@ -25,7 +26,7 @@ public class BitzTest extends BitzPlugin {
 	@Override
 	public void registerCommands() {
 
-		new Command("nameitem", (sender, command, cmd, args) -> {
+		Command base = new Command("nameitem", (sender, command, cmd, args) -> {
 
 			if (!CommandUtils.isPlayer(sender)) {
 				messenger.playerMessage(sender, BitzMessages.ONLY_PLAYERS.msg);
@@ -34,11 +35,40 @@ public class BitzTest extends BitzPlugin {
 
 			Player p = (Player) sender;
 
-			p.getInventory().addItem(ItemUtils.makeItem("nether star, named&cWelcome &b" + p.getName() + "&b!"));
+			p.getInventory().addItem(ItemUtils.makeItem("nether star, named &cWelcome &b" + p.getName() + "&b!"));
 
 			return true;
 
 		}).executorFor(getCommand("nameitem"));
+
+		Command child = base.addSubcommand("custom", this::subCommand).setUsage("/nameitem custom <type>");
+
+	}
+
+	public boolean subCommand(CommandSender sender, org.bukkit.command.Command command, String cmd, String[] args) {
+
+		if (!CommandUtils.isPlayer(sender)) {
+			messenger.playerMessage(sender, BitzMessages.ONLY_PLAYERS.msg);
+			return true;
+		}
+
+		if (args.length < 1) {
+
+			messenger.playerMessage(sender, BitzMessages.NO_ARG.msg, "type");
+			return false;
+
+		}
+
+		if (ItemUtils.getType(args[0]) == null) {
+			messenger.playerInfo(sender, "Unknown type: '" + args[0] + "'");
+			return false;
+		}
+
+		Player p = (Player) sender;
+
+		p.getInventory().addItem(ItemUtils.makeItem(args[0] + ", named &cWelcome &b" + p.getName() + "&b!"));
+
+		return true;
 
 	}
 
