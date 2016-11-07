f382670ac1e7db727d07bd0cecc74ca70b869681
Rayzr522 <rayzr522@gmail.com>
2016-11-07 08:27:49 -0500

    Updated formatting

--- src/main/java/com/rayzr522/bitztest/BitzTest.java
+++ src/main/java/com/rayzr522/bitztest/BitzTest.java
@@ -1,136 +1,267 @@
 
 package com.rayzr522.bitztest;
 
+import static mirror.Mirror.$;
+import static org.bukkit.ChatColor.BLUE;
+import static org.bukkit.ChatColor.GREEN;
+
 import java.util.Random;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import com.rayzr522.bitzapi.BitzPlugin;
 import com.rayzr522.bitzapi.message.BitzMessages;
 import com.rayzr522.bitzapi.testcommands.Command;
 import com.rayzr522.bitzapi.utils.CommandUtils;
+import com.rayzr522.bitzapi.utils.Reflection;
+import com.rayzr522.bitzapi.utils.data.LoreData;
 import com.rayzr522.bitzapi.utils.item.ItemUtils;
 
-import net.md_5.bungee.api.ChatColor;
+import mirror.PacketBuilder;
+import mirror.PlayerWrapper;
+import mkremins.fanciful.FancyMessage;
 
 public class BitzTest extends BitzPlugin {
 
-	@Override
-	public void onPluginLoad() {
+    @Override
+    public void onPluginLoad() {
 
-		messenger.setPrefix("&8&l(&7{name}&8&l)&r");
+        messenger.setPrefix("&8&l(&7{name}&8&l)&r");
 
-		configManager.ensureFolderExists(configManager.getFile("players/"));
+        configManager.ensureFolderExists(configManager.getFile("players/"));
 
-	}
+        // haxxTheBukkit();
 
-	@Override
-	public void onPluginUnload() {
-	}
+    }
 
-	@Override
-	public void registerCommands() {
+    public void haxxTheBukkit() {
 
-		Command base = new Command("nameitem", (sender, command, cmd, args) -> {
+        try {
+            Class<?> ITEM = Reflection.getNMS("Item");
+            Class<?> MINECRAFT_KEY = Reflection.getNMS("MinecraftKey");
+            Object REGISTRY = ITEM.getField("REGISTRY").get(null);
 
-			if (!CommandUtils.isPlayer(sender)) {
-				messenger.playerMessage(sender, BitzMessages.ONLY_PLAYERS.msg);
-				return true;
-			}
+            Object key = MINECRAFT_KEY.getConstructor(String.class).newInstance("minecraft:diamond_sword");
+            Object item = $(REGISTRY).getMethod("get", Object.class).invoke(key);
 
-			Player p = (Player) sender;
+            $(item).setField("maxStackSize", 3);
 
-			p.getInventory().addItem(ItemUtils.makeItem("nether star, named &cWelcome &b" + p.getName() + "&b!"));
+        } catch (Exception e) {
+            System.out.println("Failed to haxx the Bukkit");
+            e.printStackTrace();
+        }
+    }
 
-			return true;
+    @Override
+    public void onPluginUnload() {
+    }
 
-		}).executorFor(getCommand("nameitem"));
+    @Override
+    public void registerCommands() {
 
-		base.addSubcommand("custom", this::subCommand).setUsage("/nameitem custom <type>");
+        Command base = new Command("nameitem", (sender, command, cmd, args) -> {
 
-		new Command("playerInfo", (sender, command, cmd, args) -> {
+            if (!CommandUtils.isPlayer(sender)) {
+                messenger.playerMessage(sender, BitzMessages.ONLY_PLAYERS.msg);
+                return true;
+            }
 
-			if (!CommandUtils.isPlayer(sender)) {
+            Player p = (Player) sender;
 
-				messenger.playerMessage(sender, BitzMessages.ONLY_PLAYERS.msg);
-				return true;
+            ItemStack item = ItemUtils.makeItem("nether star, named &cWelcome &b" + p.getName() + "&b!");
 
-			}
+            p.getInventory().addItem(item);
 
-			Player p = (Player) sender;
+            return true;
 
-			if (args.length >= 1) {
+        }).executorFor(getCommand("nameitem"));
 
-				if (args[0].equalsIgnoreCase("load")) {
+        base.addSubcommand("custom", this::subCommand).setUsage("/nameitem custom <type>");
 
-					try {
+        new Command("watch", (sender, command, cmd, args) -> {
 
-						PlayerInfo info = configManager.load(PlayerInfo.class, "players/" + p.getUniqueId() + ".yml");
+            if (!CommandUtils.isPlayer(sender)) {
 
-						p.sendMessage(ChatColor.GREEN + "Info: " + ChatColor.BLUE + info.toString());
+                messenger.playerMessage(sender, BitzMessages.ONLY_PLAYERS.msg);
+                return true;
 
-					} catch (Exception e) {
-						sender.sendMessage(ChatColor.RED + "Failed to load player info file");
-						e.printStackTrace();
-					}
+            }
 
-				} else {
+            Player p = (Player) sender;
+            PlayerWrapper wrapper = $(p);
 
-					sender.sendMessage(ChatColor.RED + "Unknown command '" + args[0] + "'");
+            if (args.length < 1) {
 
-				}
+                Object packet = new PacketBuilder("PlayOutCamera").set("a", p.getEntityId()).create();
+                wrapper.sendPacket(packet);
 
-				return true;
+                p.sendMessage(ChatColor.RED + "Please specify a player!");
+                return true;
 
-			}
+            }
 
-			// PlayerInfo info = new PlayerInfo((Player) sender);
-			PlayerInfo info = new PlayerInfo();
+            Player target = Bukkit.getPlayer(args[0]);
 
-			info.uuid = p.getUniqueId().toString();
-			info.money = Math.abs(new Random().nextGaussian() * 50.0);
-			info.pos = p.getLocation().toVector();
+            if (target == null) {
 
-			try {
-				configManager.save(info, "players/" + info.uuid + ".yml");
-				p.sendMessage(ChatColor.DARK_PURPLE + "Data saved to players/" + info.uuid + ".yml");
-			} catch (Exception e) {
-				p.sendMessage(ChatColor.RED + "An error has occured! Please check the console.");
-				e.printStackTrace();
-			}
+                p.sendMessage(ChatColor.RED + "That player could not be found!");
+                return true;
 
-			return true;
+            }
 
-		}).executorFor(getCommand("playerInfo"));
+            Object packet = new PacketBuilder("PlayOutCamera").set("a", target.getEntityId()).create();
 
-	}
+            wrapper.sendPacket(packet);
 
-	public boolean subCommand(CommandSender sender, org.bukkit.command.Command command, String cmd, String[] args) {
+            return true;
 
-		if (!CommandUtils.isPlayer(sender)) {
-			messenger.playerMessage(sender, BitzMessages.ONLY_PLAYERS.msg);
-			return true;
-		}
+        }).executorFor(getCommand("watch"));
 
-		if (args.length < 1) {
+        new Command("playerInfo", (sender, command, cmd, args) -> {
 
-			messenger.playerMessage(sender, BitzMessages.NO_ARG.msg, "type");
-			return false;
+            if (!CommandUtils.isPlayer(sender)) {
 
-		}
+                messenger.playerMessage(sender, BitzMessages.ONLY_PLAYERS.msg);
+                return true;
 
-		if (ItemUtils.getType(args[0]) == null) {
-			messenger.playerInfo(sender, "Unknown type: '" + args[0] + "'");
-			return false;
-		}
+            }
 
-		Player p = (Player) sender;
+            Player p = (Player) sender;
 
-		p.getInventory().addItem(ItemUtils.makeItem(args[0] + ", named &cWelcome &b" + p.getName() + "&b!"));
+            if (args.length >= 1) {
 
-		return true;
+                if (args[0].equalsIgnoreCase("load")) {
 
-	}
+                    try {
+
+                        PlayerInfo info = configManager.load(PlayerInfo.class, "players/" + p.getUniqueId() + ".yml");
+
+                        p.sendMessage(ChatColor.GREEN + "Info: " + ChatColor.BLUE + info.toString());
+
+                    } catch (Exception e) {
+                        sender.sendMessage(ChatColor.RED + "Failed to load player info file");
+                        e.printStackTrace();
+                    }
+
+                } else {
+
+                    sender.sendMessage(ChatColor.RED + "Unknown command '" + args[0] + "'");
+
+                }
+
+                return true;
+
+            }
+
+            // PlayerInfo info = new PlayerInfo((Player) sender);
+            PlayerInfo info = new PlayerInfo();
+
+            info.uuid = p.getUniqueId().toString();
+            info.money = Math.abs(new Random().nextGaussian() * 50.0);
+            info.pos = p.getLocation().toVector();
+
+            $(p).sendRaw(new FancyMessage("Welcome ").color(GREEN).then(p.getDisplayName()).color(BLUE).then(" to the server!").color(GREEN).tooltip("A stupiod test server").toJSONString());
+
+            try {
+                configManager.save(info, "players/" + info.uuid + ".yml");
+                p.sendMessage(ChatColor.DARK_PURPLE + "Data saved to players/" + info.uuid + ".yml");
+            } catch (Exception e) {
+                p.sendMessage(ChatColor.RED + "An error has occured! Please check the console.");
+                e.printStackTrace();
+            }
+
+            return true;
+
+        }).executorFor(getCommand("playerInfo"));
+
+        new Command("debug", (sender, command, label, args) -> {
+
+            if (!CommandUtils.isPlayer(sender)) {
+
+                messenger.playerMessage(sender, BitzMessages.ONLY_PLAYERS.msg);
+                return true;
+
+            }
+
+            Player p = (Player) sender;
+
+            if (args.length > 0 && args[0].equalsIgnoreCase("item")) {
+
+                ItemStack item = ItemUtils.makeItem("diamond, named &b&lTest &c&nItem!");
+
+                LoreData data = new LoreData(item);
+                data.set("testData", p.getHealth());
+                data.write(item);
+
+                p.getInventory().addItem(item);
+
+                return true;
+
+            }
+
+            if (!ItemUtils.isEmpty(p.getInventory().getItemInMainHand())) {
+
+                ItemStack item = p.getInventory().getItemInMainHand();
+
+                LoreData data = new LoreData(item);
+
+                data.debug(p);
+
+            }
+
+            return true;
+
+        }).executorFor(getCommand("debug"));
+
+        new Command("rp", (sender, command, label, args) -> {
+
+            if (!CommandUtils.isPlayer(sender)) {
+
+                messenger.playerMessage(sender, BitzMessages.ONLY_PLAYERS.msg);
+                return true;
+
+            }
+
+            Player p = (Player) sender;
+
+            String path = configManager.getFile("ItemRarity.zip").getAbsolutePath();
+            p.sendMessage("Setting resource pack: " + path);
+            p.setResourcePack(path);
+
+            return true;
+        }).executorFor(getCommand("rp"));
+
+    }
+
+    public boolean subCommand(CommandSender sender, org.bukkit.command.Command command, String cmd, String[] args) {
+
+        if (!CommandUtils.isPlayer(sender)) {
+            messenger.playerMessage(sender, BitzMessages.ONLY_PLAYERS.msg);
+            return true;
+        }
+
+        if (args.length < 1) {
+
+            messenger.playerMessage(sender, BitzMessages.NO_ARG.msg, "type");
+            return false;
+
+        }
+
+        if (ItemUtils.getType(args[0]) == null) {
+            messenger.playerInfo(sender, "Unknown type: '" + args[0] + "'");
+            return false;
+        }
+
+        Player p = (Player) sender;
+
+        p.getInventory().addItem(ItemUtils.makeItem(args[0] + ", named &cWelcome &b" + p.getName() + "&b!"));
+
+        return true;
+
+    }
 
 }
--- src/main/java/com/rayzr522/bitztest/PlayerInfo.java
+++ src/main/java/com/rayzr522/bitztest/PlayerInfo.java
@@ -8,32 +8,32 @@ import com.rayzr522.bitzapi.config.data.Serialized;
 
 public class PlayerInfo implements ISerializable {
 
-	@Override
-	public void onDeserialize() {
-	}
+    @Override
+    public void onDeserialize() {
+    }
 
-	@Override
-	public void onPreSerialize() {
-	}
+    @Override
+    public void onPreSerialize() {
+    }
 
-	@Serialized
-	public double	money	= 10.0;
+    @Serialized
+    public double  money   = 10.0;
 
-	@Serialized
-	public boolean	godMode	= false;
+    @Serialized
+    public boolean godMode = false;
 
-	@Serialized
-	public String	nickName;
+    @Serialized
+    public String  nickName;
 
-	@Serialized
-	public String	uuid;
+    @Serialized
+    public String  uuid;
 
-	@Serialized
-	public Vector	pos;
+    @Serialized
+    public Vector  pos;
 
-	@Override
-	public String toString() {
-		return "PlayerInfo [money=" + money + ", godMode=" + godMode + ", nickName=" + nickName + ", uuid=" + uuid + ", pos=" + pos + "]";
-	}
+    @Override
+    public String toString() {
+        return "PlayerInfo [money=" + money + ", godMode=" + godMode + ", nickName=" + nickName + ", uuid=" + uuid + ", pos=" + pos + "]";
+    }
 
 }
